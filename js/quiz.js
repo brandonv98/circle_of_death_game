99fc3f703d602c7c36f7138cf7ed204a8c5cee3e
Brandon VanCamp <brandon@overtimewebdev.com>
2018-03-24 22:55:43 -0500

    n/a

--- js/quiz.js
+++ js/quiz.js
@@ -45,7 +45,7 @@ const handleDraw = () => {
 		console.log('Random Card is : ', card, 'Random Number : ', randomCard());
 		if (card === randomCard()) {
 			h1.innerHTML = 'You Lost..';
-			liCard.innerHTML = 'DRINK BITCH!';
+			liCard.innerHTML = 'DRINK!';
 			button.setAttribute('disabled', true);
 			button.setAttribute('style', 'background-color: lightgray;');
 		}
